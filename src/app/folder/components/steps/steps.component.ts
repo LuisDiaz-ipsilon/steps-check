9f17fae98444fab3eaf024145ae9abf21f664b96
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2023-06-29 19:11:06 -0600

    add chart Cauge

--- src/app/folder/components/steps/steps.component.ts
+++ src/app/folder/components/steps/steps.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Color, ScaleType } from '@swimlane/ngx-charts';
+
 @Component({
   selector: 'app-steps',
   templateUrl: './steps.component.html',
@@ -7,7 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StepsComponent  implements OnInit {
 
-  constructor() { }
+
+  view: [number, number] = [500, 250];
+  legend: boolean = true;
+  legendPosition: any = 'below';
+
+  colorScheme: Color = { 
+    domain: ['#99CCE5', '#FF7F7F'], 
+    group: ScaleType.Ordinal, 
+    selectable: true, 
+    name: 'Customer Usage', 
+  };
+
+  single = [
+    {
+      "name": "Germany",
+      "value": 100
+    }
+  ];
+
+  constructor() {
+    //Object.assign(this, { this.single });
+  }
+
+  onSelect(data : any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data : any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data : any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 
   ngOnInit() {}
 
